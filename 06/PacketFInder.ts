0cb68951599a969766fe82e4c958cd1e1e799629
andrew <andrew_gallacher@hotmail.com>
2022-12-06 17:49:01 +0000

    DRY for day 6

--- 06/PacketFInder.ts
+++ 06/PacketFInder.ts
@@ -1,11 +1,29 @@
 export class PacketFinder {
-  packetLength: number;
+  markerLength: number;
 
-  constructor(packetLength: number) {
-    this.packetLength = packetLength;
+  constructor(markerLength: number) {
+    this.markerLength = markerLength;
   }
 
-  find(data: string[]): number {
-    return 0;
+  find(stream: string[]): number {
+    let currentIndex = this.markerLength - 1;
+    while (currentIndex < stream.length) {
+      let isStartOfPacket = true;
+      let packet: string[] = [stream[currentIndex]];
+      for (let offset = 1 - this.markerLength; offset < 0; offset++) {
+        if (packet.includes(stream[currentIndex + offset])) {
+          isStartOfPacket = false;
+        } else {
+          packet.push(stream[currentIndex + offset]);
+        }
+      }
+
+      currentIndex++;
+      if (isStartOfPacket) {
+        return currentIndex;
+      }
+    }
+
+    return -1;
   }
 }
--- 06/Part1.ts
+++ 06/Part1.ts
@@ -1,28 +1,11 @@
-// Day 6 (Part 1)
+// Day 6 (Part 2)
 import { IPuzzle } from "../src/IPuzzle";
+import { PacketFinder } from "./PacketFInder";
 
 export class Day06Part1 implements IPuzzle<number> {
   solve(data: string[]): number {
     const stream = data[0].split("");
-
-    let currentIndex = 3;
-    while (currentIndex < stream.length) {
-      let isStartOfPacket = true;
-      let packet: string[] = [stream[currentIndex]];
-      for (let offset = -3; offset < 0; offset++) {
-        if (packet.includes(stream[currentIndex + offset])) {
-          isStartOfPacket = false;
-        } else {
-          packet.push(stream[currentIndex + offset]);
-        }
-      }
-
-      currentIndex++;
-      if (isStartOfPacket) {
-        return currentIndex;
-      }
-    }
-
-    return -1;
+    const finder = new PacketFinder(4);
+    return finder.find(stream);
   }
 }
