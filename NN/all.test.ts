bf1bffa51cf693a85694a1e15b0a1863d72ceeab
andrew <andrew_gallacher@hotmail.com>
2022-12-09 09:16:44 +0000

    Add main input to test template

--- NN/all.test.ts
+++ NN/all.test.ts
@@ -5,6 +5,7 @@ import { DayNNPart2 } from "./Part2";
 const part1 = new DayNNPart1();
 const part2 = new DayNNPart2();
 const sample = getData("NN/sample");
+const input = getData("NN/input");
 
 describe("Day NN sample", () => {
   test("part1 (main sample)", () => {
@@ -14,6 +15,18 @@ describe("Day NN sample", () => {
 
   test("part2 (main sample)", () => {
     const result = part2.solve(sample);
+    expect(result).toBe(-1);
+  });
+});
+
+describe("Day NN input", () => {
+  test("part1", () => {
+    const result = part1.solve(input);
+    expect(result).toBe(-1);
+  });
+
+  test("part2", () => {
+    const result = part2.solve(input);
     expect(result).toBe(-1);
   });
 });
