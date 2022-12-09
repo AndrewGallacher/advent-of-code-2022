ac9933cf79094c69da2558bf6ac534dae8a93663
andrew <andrew_gallacher@hotmail.com>
2022-12-09 09:15:05 +0000

    More tests for day 8

--- 08/day8.test.ts
+++ 08/day8.test.ts
@@ -5,6 +5,7 @@ import { Day08Part2 } from "./Part2";
 const part1 = new Day08Part1();
 const part2 = new Day08Part2();
 const sample = getData("08/sample");
+const input = getData("08/input");
 
 describe("Day 08 sample", () => {
   test("part1 (main sample)", () => {
@@ -15,5 +16,17 @@ describe("Day 08 sample", () => {
   test("part2 (main sample)", () => {
     const result = part2.solve(sample);
     expect(result).toBe(8);
+  });
+});
+
+describe("Day 08 input", () => {
+  test("part1", () => {
+    const result = part1.solve(input);
+    expect(result).toBe(1546);
+  });
+
+  test("part2", () => {
+    const result = part2.solve(input);
+    expect(result).toBe(519064);
   });
 });
